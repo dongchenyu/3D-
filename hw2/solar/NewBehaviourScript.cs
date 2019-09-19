b48bb02f13563244f374acff55335b5cf788d3f7
dongchenyu <34881087+dongchenyu@users.noreply.github.com>
2019-09-19 13:42:44 +0800

    Add files via upload

---
+++ hw2/solar/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform sun;
+    private float random_x, random_y;
+    private void Start()
+    {
+        random_x = Random.Range(1, 10);
+        random_y = Random.Range(10, 60);
+    }
+    void Update()
+    {
+        Vector3 axis = new Vector3(random_x, random_y, 0);
+        this.transform.RotateAround(sun.position, axis, 15 * Time.deltaTime);
+    }
+}
